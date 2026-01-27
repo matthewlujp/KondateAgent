45ac8216770ae16c16e180eea68045d110afeff6
Matthew Ishige <matthewlujp@gmail.com>
2026-01-27 21:56:48 +0800

    feat: add SettingsPage with channel management and placeholder sections

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -9,3 +9,7 @@ export { TextInputFallback } from './TextInputFallback';
 export { ConfirmationFooter } from './ConfirmationFooter';
 export { SessionStartModal } from './SessionStartModal';
 export { RecipeSearchProgress } from './RecipeSearchProgress';
+export { ChannelCard } from './ChannelCard';
+export { AddChannelInput } from './AddChannelInput';
+export { ChannelManagementSection } from './ChannelManagementSection';
+export { PlaceholderSection } from './PlaceholderSection';
